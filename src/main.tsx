29d4aa886cbae72223af8212eca94d809d6d4a60
John Rees <john@bitsushi.com>
2021-07-13 15:47:03 +0100

    yarn storybook

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom";
+import Map from "./Map";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Map />
   </React.StrictMode>,
-  document.getElementById('root')
-)
+  document.getElementById("root")
+);
